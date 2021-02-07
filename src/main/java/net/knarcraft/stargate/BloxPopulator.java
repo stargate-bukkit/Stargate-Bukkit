9233776b2cad938e7141e6c2876aad1a20466053
EpicKnarvik97 <kristian.knarvik@knett.no>
2021-02-08 00:32:20 +0100

    Adds comments to BloxPopulator

--- src/main/java/net/knarcraft/stargate/BloxPopulator.java
+++ src/main/java/net/knarcraft/stargate/BloxPopulator.java
@@ -3,44 +3,82 @@ package net.knarcraft.stargate;
 import org.bukkit.Axis;
 import org.bukkit.Material;
 
+/**
+ * Used to store information about a custom block populator
+ */
 public class BloxPopulator {
 
     private BlockLocation blockLocation;
     private Material nextMat;
     private Axis nextAxis;
 
-    public BloxPopulator(BlockLocation b, Material m) {
-        blockLocation = b;
-        nextMat = m;
+    /**
+     * Instantiates a new block populator
+     * @param blockLocation <p>The location to start from</p>
+     * @param material <p>The material to populate</p>
+     */
+    public BloxPopulator(BlockLocation blockLocation, Material material) {
+        this.blockLocation = blockLocation;
+        nextMat = material;
         nextAxis = null;
     }
 
-    public BloxPopulator(BlockLocation b, Material m, Axis a) {
-        blockLocation = b;
-        nextMat = m;
-        nextAxis = a;
+    /**
+     * Instantiates a new block populator
+     * @param blockLocation <p>The location to start from</p>
+     * @param material <p>The material to populate</p>
+     * @param axis <p>The axis to populate along</p>
+     */
+    public BloxPopulator(BlockLocation blockLocation, Material material, Axis axis) {
+        this.blockLocation = blockLocation;
+        nextMat = material;
+        nextAxis = axis;
     }
 
-    public void setBlockLocation(BlockLocation b) {
-        blockLocation = b;
+    /**
+     * Sets the location to start from
+     * @param blockLocation <p>The new start location</p>
+     */
+    public void setBlockLocation(BlockLocation blockLocation) {
+        this.blockLocation = blockLocation;
     }
 
-    public void setMat(Material m) {
-        nextMat = m;
+    /**
+     * Sets the polulator material
+     * @param material <p>The new populator material</p>
+     */
+    public void setMat(Material material) {
+        nextMat = material;
     }
 
-    public void setAxis(Axis a) {
-        nextAxis = a;
+    /**
+     * Sets the populator axis
+     * @param axis <p>The new populator axis</p>
+     */
+    public void setAxis(Axis axis) {
+        nextAxis = axis;
     }
 
+    /**
+     * Gets the location to start from
+     * @return <p>The location to start from</p>
+     */
     public BlockLocation getBlockLocation() {
         return blockLocation;
     }
 
+    /**
+     * Gets the material used for population
+     * @return <p>The material used for population</p>
+     */
     public Material getMat() {
         return nextMat;
     }
 
+    /**
+     * Gets the current population axis
+     * @return <p>The current population axis</p>
+     */
     public Axis getAxis() {
         return nextAxis;
     }
