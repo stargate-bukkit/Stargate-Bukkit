05ed2254c2de466084f6417da1b5ba6fad722945
Thorinwasher <ragnarlol96@gmail.com>
2021-06-07 17:47:35 +0200

    Minor changes in planning for PluginEventListener

--- src/main/java/net/TheDgtl/Stargate/listeners/PluginEventListener.java
+++ src/main/java/net/TheDgtl/Stargate/listeners/PluginEventListener.java
@@ -1,12 +1,18 @@
 package net.TheDgtl.Stargate.listeners;
 
+import java.util.logging.Level;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 
+import net.TheDgtl.Stargate.Stargate;
+
 public class PluginEventListener implements Listener{
+	
+	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPluginEnable(PluginEnableEvent event) {
 		//check if vaults
@@ -15,8 +21,9 @@ public class PluginEventListener implements Listener{
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPluginDisable(PluginDisableEvent event) {
 		//check if the plugin was the economyhandler and send a message
-		if (event.getPlugin().equals(stargate.getEconomyHandler().getVault())) {
-            stargate.getStargateLogger().info("[Stargate] Vault plugin lost.");
-        }
+		/*
+		 * if (event.getPlugin().equals(stargate.getEconomyHandler().getVault())) {
+		 * Stargate.log("Vault plugin lost.", Level.INFO); }
+		 */
 	}
 }
