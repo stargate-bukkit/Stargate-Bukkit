660cf89edcdb5172109e45d62c2c1e3a7afcbf29
Thorinwasher <ragnarlol96@gmail.com>
2021-07-04 20:00:57 +0200

    Create PermissionManager.java

---
+++ src/main/java/net/TheDgtl/Stargate/PermissionManager.java
@@ -0,0 +1,10 @@
+package net.TheDgtl.Stargate;
+
+import org.bukkit.entity.Player;
+
+public class PermissionManager {
+	private Player player;
+	public PermissionManager(Player player) {
+		this.player = player;
+	}
+}
