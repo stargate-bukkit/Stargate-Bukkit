a02bfbe89551a309bbe2969f3fc6bcf170afa92d
Thorinwasher <ragnarlol96@gmail.com>
2021-07-30 20:12:57 +0200

    Added mysql/mariadb/sqlite compatability
    Mainly just taken from another plugin (ParamnesticCure). I did some changes to make the class less messy (who cares)

---
+++ src/main/java/net/TheDgtl/Stargate/database/SqlManager.java
@@ -0,0 +1,133 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package net.TheDgtl.Stargate.database;
+
+
+
+
+import static org.bukkit.Bukkit.getPluginManager;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import net.TheDgtl.Stargate.Stargate;
+
+
+/**
+ * @author Frostalf
+ * @author Thorin
+ */
+public class SqlManager {
+
+    private HikariDataSource boneCP;
+    private Connection connection;
+
+    private HikariConfig config = null;
+    private File dbFile;
+    private final String database;
+    private final String address;
+    private final int port;
+    private final DriverEnum driver;
+    private String url;
+	private Stargate plugin;
+    
+    /**
+     * Caches and checks the status of a certain database..
+     * @param driver Driver to use for this database communication.
+     * @param address Address of the database being checked.
+     * @param port Port on which to communicate with the database being checked.
+     * @param database Name of the database being checked.
+     * @param plugin
+     * @throws SQLException 
+     */
+    public SqlManager(String driver, String address, int port, String database, Stargate plugin) throws SQLException {
+    	
+        this.database = database;
+        this.driver = getDriver(driver);
+        this.port = port;
+        this.address = address;
+        this.plugin = plugin;
+        
+        switch(this.driver) {
+        case SQLITE:
+        	setupSQLITE();
+        	break;
+        case MYSQL:
+        case MARIADB:
+        	setupMySQL();
+        	break;
+        default:
+        	break;
+        }
+    }
+
+    /**
+     * Gets the current database connection.
+     * @return Current connection.
+     * @throws java.sql.SQLException
+     */
+    public Connection getConnection() throws SQLException {
+    	switch(driver) {
+    	case MYSQL:
+    	case MARIADB:
+    		return connection = this.boneCP.getConnection();
+    	case SQLITE:
+    		return connection = DriverManager.getConnection(this.url);
+    	default:
+    		plugin.getLogger().log(Level.INFO, "{0} {1} {2}", new Object[]{this.url, this.database, this.connection.toString()});
+            return this.connection;
+    	}
+        
+    }
+
+    public DriverEnum getDriver(String driver) {
+        return DriverEnum.valueOf(driver.toUpperCase());
+    }
+
+    private void setupMySQL() throws SQLException {
+    	//Creates a properties file if it doesn't exist
+    	if ( !new File(plugin.getDataFolder(), "hikari.properties").exists() ) {
+    		plugin.getLogger().warning("[Startup] hikari.properties file is missing.");
+    		plugin.getLogger().info("Providing you with a new properties file");
+    		plugin.getDataFolder().mkdirs();
+    		plugin.saveResource("hikari.properties", true);
+    		getPluginManager().disablePlugin(plugin);
+    	}
+    	
+    	//creates a config based on the properties file
+        this.config = new HikariConfig(plugin.getDataFolder()+"/hikari.properties");
+        
+        
+        config.setJdbcUrl("jdbc:"+ this.driver +"://" + this.address + ":" + this.port + "/" + this.database);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SqlManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        
+        this.boneCP = new HikariDataSource(config);
+    }
+
+    private void setupSQLITE() throws SQLException {
+        dbFile = new File(plugin.getDataFolder().getAbsoluteFile(), this.database + ".db");
+
+        this.url = ("jdbc:sqlite:" + dbFile.getAbsoluteFile());
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SqlManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DriverManager.registerDriver(new org.sqlite.JDBC());
+    }
+}
