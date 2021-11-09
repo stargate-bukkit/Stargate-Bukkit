85edaa465792cb18bea0f135b91c6dd245b64593
EpicKnarvik97 <kristian.knarvik@knett.no>
2021-11-09 15:38:42 +0100

    Adds a new class to represent a data type usable by configs

---
+++ src/main/java/net/knarcraft/stargate/config/OptionDataType.java
@@ -0,0 +1,21 @@
+package net.knarcraft.stargate.config;
+
+/**
+ * An enum defining the different data types an option can have
+ */
+public enum OptionDataType {
+
+    /**
+     * The data type for the option is a String
+     */
+    STRING,
+    /**
+     * The data type for the option is a Boolean
+     */
+    BOOLEAN,
+    /**
+     * The data type for the option is an Integer
+     */
+    INTEGER
+
+}
