6466c7b0ff52860aa94cbd5ba5a035d6f693fc30
EpicKnarvik97 <kristian.knarvik@knett.no>
2021-11-09 02:04:59 +0100

    Adds the config command to the stargate auto completer
    Additionally makes the reload command only auto-complete if the command sender can use it

--- src/main/java/net/knarcraft/stargate/command/StarGateTabCompleter.java
+++ src/main/java/net/knarcraft/stargate/command/StarGateTabCompleter.java
@@ -3,6 +3,7 @@ package net.knarcraft.stargate.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,7 +20,13 @@ public class StarGateTabCompleter implements TabCompleter {
                                                 @NotNull String s, @NotNull String[] args) {
         List<String> commands = new ArrayList<>();
         commands.add("about");
-        commands.add("reload");
+        if (!(commandSender instanceof Player player) || player.hasPermission("stargate.admin.reload")) {
+            commands.add("reload");
+        }
+        if (!(commandSender instanceof Player player) || player.hasPermission("stargate.admin")) {
+            commands.add("config");
+        }
+
 
         if (args.length == 1) {
             return commands;
