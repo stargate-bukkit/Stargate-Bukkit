9aa5f6fadae9bcbfd255da7b1c885ba209953416
EpicKnarvik97 <kristian.knarvik@knett.no>
2021-12-06 21:29:29 +0100

    Adds JavaDoc to the plugin event listener

--- src/main/java/net/TheDgtl/Stargate/listeners/PluginEventListener.java
+++ src/main/java/net/TheDgtl/Stargate/listeners/PluginEventListener.java
@@ -1,30 +1,41 @@
 package net.TheDgtl.Stargate.listeners;
 
-import java.util.logging.Level;
-
+import net.TheDgtl.Stargate.Stargate;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 
-import net.TheDgtl.Stargate.Stargate;
+import java.util.logging.Level;
 
+/**
+ * Listens for and sets up any relevant plugins being loaded
+ */
 public class PluginEventListener implements Listener {
 
-
+    /**
+     * Listens for any valid economy plugins being loaded and sets up economy if necessary
+     *
+     * @param event <p>The triggered plugin enable event</p>
+     */
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPluginEnable(PluginEnableEvent event) {
-        if(Stargate.economyManager.isValidEconomyPlugin(  event.getPlugin())) {
+        if (Stargate.economyManager.isValidEconomyPlugin(event.getPlugin())) {
             Stargate.economyManager.setEconomy(event.getPlugin());
         }
-
     }
 
+    /**
+     * Listens for the economy manager plugin being unloaded and informs the console
+     *
+     * @param event <p>The triggered plugin unload event</p>
+     */
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPluginDisable(PluginDisableEvent event) {
         if (event.getPlugin().equals(Stargate.economyManager.getEconomyPlugin())) {
-            Stargate.log(Level.INFO,"Vault plugin lost."); 
+            Stargate.log(Level.INFO, "Vault plugin lost.");
         }
     }
+
 }
