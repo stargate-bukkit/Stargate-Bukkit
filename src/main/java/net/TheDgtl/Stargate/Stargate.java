cb6ec864cac6c2d17f4bffd61bee48c1f0bb3458
Thorinwasher <ragnarlol96@gmail.com>
2021-12-15 19:34:53 +0100

    Fixed another stacktrace when config is not available on startup

--- src/main/java/net/TheDgtl/Stargate/Stargate.java
+++ src/main/java/net/TheDgtl/Stargate/Stargate.java
@@ -122,6 +122,8 @@ public class Stargate extends JavaPlugin implements StargateLogger {
     @Override
     public void onEnable() {
         instance = this;
+        if(!new File(this.getDataFolder(),"config.yml").exists())
+            super.saveDefaultConfig();
 
         if (Settings.getInteger(Setting.CONFIG_VERSION) != CURRENT_CONFIG_VERSION) {
             try {
@@ -130,6 +132,7 @@ public class Stargate extends JavaPlugin implements StargateLogger {
                 e.printStackTrace();
             }
         }
+        
         
         saveDefaultGates();
         
