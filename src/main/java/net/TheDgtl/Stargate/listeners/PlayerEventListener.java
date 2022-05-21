f92bd8ebab8db1d2a938c1fdb9413fc1e8a62508
Thorinwasher <ragnarlol96@gmail.com>
2022-05-21 17:16:07 +0200

    Avoids a stacktrace

--- src/main/java/net/TheDgtl/Stargate/listeners/PlayerEventListener.java
+++ src/main/java/net/TheDgtl/Stargate/listeners/PlayerEventListener.java
@@ -134,7 +134,7 @@ public class PlayerEventListener implements Listener {
      */
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (!ConfigurationHelper.getBoolean(ConfigurationOption.USING_BUNGEE) && ConfigurationHelper.getBoolean(ConfigurationOption.USING_REMOTE_DATABASE)) {
+        if (!ConfigurationHelper.getBoolean(ConfigurationOption.USING_BUNGEE) || !ConfigurationHelper.getBoolean(ConfigurationOption.USING_REMOTE_DATABASE)) {
             return;
         }
 
