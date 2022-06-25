32647920f7bc8d4113a5d4c2376d8febd6becf8f
EpicKnarvik97 <kristian.knarvik@knett.no>
2022-06-25 23:02:54 +0200

    Cleans the end portal iris workaround code

--- src/main/java/net/TheDgtl/Stargate/listener/MoveEventListener.java
+++ src/main/java/net/TheDgtl/Stargate/listener/MoveEventListener.java
@@ -18,7 +18,9 @@ import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 
 /**
@@ -98,43 +100,7 @@ public class MoveEventListener implements Listener {
         RealPortal portal = null;
         if (toLocation != null && toLocation.getWorld() != null &&
                 toLocation.getWorld().getEnvironment() == World.Environment.THE_END) {
-            List<Location> relevantLocations = new ArrayList<>();
-            Vector targetVelocity = normalizeVelocity(toLocation.toVector().subtract(fromLocation.toVector()));
-            if (targetVelocity.getX() != 0 && targetVelocity.getY() != 0 && targetVelocity.getZ() != 0) {
-                relevantLocations.add(toLocation.clone().add(targetVelocity));
-            }
-            if (targetVelocity.getX() != 0 && targetVelocity.getZ() != 0) {
-                relevantLocations.add(toLocation.clone().add(new Vector(targetVelocity.getX(), 0, targetVelocity.getZ())));
-            }
-            if (targetVelocity.getX() != 0 && targetVelocity.getY() != 0) {
-                relevantLocations.add(toLocation.clone().add(new Vector(targetVelocity.getX(), targetVelocity.getY(), 0)));
-            }
-            if (targetVelocity.getZ() != 0 && targetVelocity.getY() != 0) {
-                relevantLocations.add(toLocation.clone().add(new Vector(0, targetVelocity.getY(), targetVelocity.getZ())));
-            }
-            if (targetVelocity.getX() != 0) {
-                relevantLocations.add(toLocation.clone().add(new Vector(targetVelocity.getX(), 0, 0)));
-            }
-            if (targetVelocity.getY() != 0) {
-                relevantLocations.add(toLocation.clone().add(new Vector(0, targetVelocity.getY(), 0)));
-            }
-            if (targetVelocity.getZ() != 0) {
-                relevantLocations.add(toLocation.clone().add(new Vector(0, 0, targetVelocity.getZ())));
-            }
-            for (Location headingTo : relevantLocations) {
-                RealPortal possiblePortal = Stargate.getRegistryStatic().getPortal(headingTo, GateStructureType.IRIS);
-                if (possiblePortal != null && 
-                        possiblePortal.getGate().getFormat().getIrisMaterial(true) == Material.END_PORTAL) {
-                    Location middle = new Location(headingTo.getWorld(), headingTo.getBlockX() + 0.5,
-                            headingTo.getBlockY() + 0.5, headingTo.getBlockZ() + 0.5);
-                    if (Math.abs(middle.getX() - toLocation.getX()) < 0.6 ||
-                            Math.abs(middle.getY() - toLocation.getY()) < 0.6 ||
-                            Math.abs(middle.getZ() - toLocation.getZ()) < 0.6) {
-                        portal = possiblePortal;
-                        break;
-                    }
-                }
-            }
+            portal = getAdjacentEndPortalStargate(fromLocation, toLocation);
         }
 
         // Check if entity moved one block (its only possible to have entered a portal if that's the case)
@@ -157,6 +123,66 @@ public class MoveEventListener implements Listener {
         Stargate.log(Level.FINER, "Trying to teleport entity, initial velocity: " + target.getVelocity() +
                 ", new velocity: " + newVelocity);
         portal.doTeleport(target);
+    }
+
+    /**
+     * Gets the first adjacent Stargate using END_PORTAL as iris, if any
+     *
+     * @param fromLocation <p>The location the target moved from</p>
+     * @param toLocation   <p>The location the target moved to</p>
+     * @return <p>The first found adjacent Stargate using END_PORTAL, or null</p>
+     */
+    private RealPortal getAdjacentEndPortalStargate(Location fromLocation, Location toLocation) {
+        List<Location> relevantLocations = getRelevantAdjacentLocations(fromLocation, toLocation);
+        for (Location headingTo : relevantLocations) {
+            RealPortal possiblePortal = Stargate.getRegistryStatic().getPortal(headingTo, GateStructureType.IRIS);
+            if (possiblePortal != null &&
+                    possiblePortal.getGate().getFormat().getIrisMaterial(true) == Material.END_PORTAL) {
+                Location middle = new Location(headingTo.getWorld(), headingTo.getBlockX() + 0.5,
+                        headingTo.getBlockY() + 0.5, headingTo.getBlockZ() + 0.5);
+                if (Math.abs(middle.getX() - toLocation.getX()) < 0.6 ||
+                        Math.abs(middle.getY() - toLocation.getY()) < 0.6 ||
+                        Math.abs(middle.getZ() - toLocation.getZ()) < 0.6) {
+                    return possiblePortal;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets the adjacent locations relevant for END_PORTAL checking based on the given movement
+     *
+     * @param fromLocation <p>The location the target moved from</p>
+     * @param toLocation   <p>The location the target moved to</p>
+     * @return <p>The relevant adjacent locations</p>
+     */
+    private List<Location> getRelevantAdjacentLocations(Location fromLocation, Location toLocation) {
+        List<Location> relevantLocations = new ArrayList<>();
+        Vector zeroVector = new Vector();
+        Vector targetVelocity = normalizeVelocity(toLocation.toVector().subtract(fromLocation.toVector()));
+
+        //Calculate all relevant vectors that might point to end portal Stargates
+        Set<Vector> relevantVectors = new HashSet<>();
+        Vector xVector = new Vector(targetVelocity.getX(), 0, 0);
+        Vector yVector = new Vector(0, targetVelocity.getY(), 0);
+        Vector zVector = new Vector(0, 0, targetVelocity.getZ());
+        relevantVectors.add(xVector.clone());
+        relevantVectors.add(yVector.clone());
+        relevantVectors.add(zVector.clone());
+        relevantVectors.add(xVector.clone().add(yVector));
+        relevantVectors.add(xVector.clone().add(zVector));
+        relevantVectors.add(yVector.clone().add(zVector));
+        relevantVectors.add(yVector.clone().add(zVector).add(xVector));
+
+        //Calculate the locations resulting from the relevant block vectors
+        relevantVectors.forEach(relevantVector -> {
+            //It's not necessary to check the actual target block
+            if (!relevantVector.equals(zeroVector)) {
+                relevantLocations.add(toLocation.clone().add(relevantVector));
+            }
+        });
+        return relevantLocations;
     }
 
     /**
