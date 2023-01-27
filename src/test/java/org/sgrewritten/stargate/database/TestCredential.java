e6846e34d23538f8dcc72132062bf9b81bcb0e9b
EpicKnarvik97 <kristian.knarvik@knett.no>
2023-01-27 18:48:23 +0100

    Changes some naming for the test credential stuff

---
+++ src/test/java/org/sgrewritten/stargate/database/TestCredential.java
@@ -0,0 +1,15 @@
+package org.sgrewritten.stargate.database;
+
+public enum TestCredential {
+    
+    MYSQL_DB_PASSWORD,
+
+    MYSQL_DB_USER,
+
+    MYSQL_DB_PORT,
+
+    MYSQL_DB_NAME,
+
+    MYSQL_DB_ADDRESS;
+    
+}
