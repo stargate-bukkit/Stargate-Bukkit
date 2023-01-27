8126182cb4b23be459c6d2f542e2cad1cd33bbe1
Thorinwasher <ragnarlol96@gmail.com>
2023-01-27 18:21:31 +0100

    .

---
+++ src/test/java/org/sgrewritten/stargate/util/TestCredentialsHandler.java
@@ -0,0 +1,50 @@
+package org.sgrewritten.stargate.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.sgrewritten.stargate.Stargate;
+
+public class TestCredentialsHandler {
+    
+    
+    static private Properties properties = new Properties();
+    static {
+        try {
+            loadProperties();
+        } catch(IOException e) {
+            Stargate.log(e);
+        }
+    }
+    
+    
+    public static String getPassword(){
+        return properties.getProperty("DB_PASSWORD");
+    }
+    
+    public static String getUser() {
+        return properties.getProperty("DB_USER");
+    }
+    
+    public static int getPort() {
+        return Integer.valueOf(properties.getProperty("DB_PORT"));
+    }
+    
+    public static String getDatabaseName() {
+        return properties.getProperty("DB_NAME");
+    }
+    
+    public static String getAddress() {
+        return properties.getProperty("DB_ADDRESS");
+    }
+    
+    private static void loadProperties() throws IOException {
+        File location = new File("src/test/resources/credentials.secret");
+        try(InputStream stream = new FileInputStream(location)) {
+            properties.load(stream);
+        }
+    }
+}
