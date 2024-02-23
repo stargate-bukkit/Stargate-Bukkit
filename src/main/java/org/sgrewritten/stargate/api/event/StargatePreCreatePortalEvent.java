dbe48294b9c8e7585906245130244ab34528f783
Thorinwasher <ragnarlol96@gmail.com>
2024-02-23 10:19:42 +0100

    Make StargatePreCreatePortalEvent cancellable

--- src/main/java/org/sgrewritten/stargate/api/event/StargatePreCreatePortalEvent.java
+++ src/main/java/org/sgrewritten/stargate/api/event/StargatePreCreatePortalEvent.java
@@ -1,6 +1,7 @@
 package org.sgrewritten.stargate.api.event;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -13,12 +14,13 @@ import java.util.Objects;
 /**
  * <p>Is triggered before the {@link org.sgrewritten.stargate.api.event.portal.StargateCreatePortalEvent}.</p>
  */
-public class StargatePreCreatePortalEvent extends Event {
+public class StargatePreCreatePortalEvent extends Event implements Cancellable {
     private static final HandlerList handlerList = new HandlerList();
     private final PortalBuilder portalBuilder;
     private final GateBuilder gateBuilder;
     private final String[] args;
     private final Player player;
+    private boolean cancelled = false;
 
 
     /**
@@ -89,6 +91,7 @@ public class StargatePreCreatePortalEvent extends Event {
         return player;
     }
 
+
     @Override
     public @NotNull HandlerList getHandlers() {
         return handlerList;
@@ -101,5 +104,15 @@ public class StargatePreCreatePortalEvent extends Event {
      */
     public static HandlerList getHandlerList() {
         return handlerList;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 }
