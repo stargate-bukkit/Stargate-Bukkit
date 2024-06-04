9aac06b72b82712ee6b672d0717e8b94fc30f2c7
Thorinwasher <ragnarlol96@gmail.com>
2024-06-04 20:29:04 +0200

    Fix #346
    Accidentally used cur instead of copy :3

--- src/main/java/org/sgrewritten/stargate/network/portal/StargatePortal.java
+++ src/main/java/org/sgrewritten/stargate/network/portal/StargatePortal.java
@@ -215,6 +215,7 @@ public class StargatePortal implements RealPortal {
 
         Stargate.log(Level.FINE, "Closing the portal");
         redrawSigns();
+        getGate().close();
         openFor = null;
     }
 
