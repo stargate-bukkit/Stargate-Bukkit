ef132bfaa4cca9fd3a33201dff1ab6d36fdbfe6b
ZinCentimeter <zincentimeter@qq.com>
2024-06-10 02:05:04 +0800

    Add SQL statements for MySQL, fix SQL Syntax Exception on MySQL 8.0.30 stargate-rewritten/Stargate-Bukkit#369

---
+++ src/main/resources/database/mysql-queries/CREATE_TABLE_INTER_PORTAL_POSITION.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS {InterPortalPosition}
+(
+   portalName NVARCHAR (180) NOT NULL,
+   networkName NVARCHAR (180) NOT NULL,
+   xCoordinate INTEGER NOT NULL,
+   yCoordinate INTEGER NOT NULL,
+   zCoordinate INTEGER NOT NULL,
+   positionType INTEGER NOT NULL,
+   metaData TEXT,
+   pluginName VARCHAR(255) DEFAULT 'Stargate',
+   PRIMARY KEY
+   (
+      portalName,
+      networkName,
+      xCoordinate,
+      yCoordinate,
+      zCoordinate
+   ),
+   FOREIGN KEY
+   (
+      portalName,
+      networkName
+   )
+   REFERENCES {InterPortal}
+   (
+      name,
+      network
+   )
+   ON UPDATE CASCADE,
+   FOREIGN KEY (positionType) REFERENCES {PositionType} (id)
+);
